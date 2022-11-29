604c7673bc1850dfc14c5fb7c2ee75d1a9c6481f
DavidDccs <david.castro001@edu.pucrs.br>
2022-11-29 02:36:14 -0300

    Realizado transporte

--- TF Poo/src/Transporte.java
+++ TF Poo/src/Transporte.java
@@ -8,12 +8,33 @@ public abstract class Transporte {
 
 	private EspacoPorto destino;
 
-	public Transporte(int identificador, EspacoPorto origem, EspacoPorto destino) {
+	private double custo;
 
+	private String estado;
+
+	private static EspacoPorto terra = new EspacoPorto(1, "Terra", 0,0,0);
+
+	public Transporte(int identificador, EspacoPorto origem, EspacoPorto destino) {
+		this.identificador = identificador;
+		this.origem = origem;
+		this.destino = destino;
+		estado = "Pendente";
 	}
+
+	public Transporte(int identificador, EspacoPorto destino){
+		this.identificador = identificador;
+		this.origem = terra;
+		this.destino = destino;
+		estado = "Pendente";
+	}
+
+
 
 	public abstract double calculaDistancia();
 
 	public abstract double calculaCusto();
 
+	public static EspacoPorto getTerra() {
+		return terra;
+	}
 }
